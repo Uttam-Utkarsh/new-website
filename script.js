2452daddead99dd1dcbcd15ba15bb766311e0c33
Uttam Utkarsh <130544853+Uttam-Utkarsh@users.noreply.github.com>
2024-05-07 23:55:09 +0530

    page3 completed

--- script.js
+++ script.js
@@ -17,5 +17,24 @@ function slider1(){
       });
 }
 
+function slider2(){
+  var swiper2 = new swiper(".page3 .mySwiper2", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination2",
+    },
+  });
+}
 
 slider1();
+slider2();
