8ce636594081fae7bbec60cebded59115bdad484
Uttam Utkarsh <130544853+Uttam-Utkarsh@users.noreply.github.com>
2024-05-09 20:08:22 +0530

    page4 completed

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function slider1(){
-    var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        // pagination: {
-        //   el: ".swiper-pagination",
-        //   clickable: true,
-        // },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 }
 
 function slider2(){
@@ -37,4 +37,4 @@ function slider2(){
 }
 
 slider1();
-slider2();
+// slider2();
